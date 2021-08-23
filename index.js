85e91f068f4618b8a64cdfb136819361044a225b
hir0o <mk.9.q.kyu@gmail.com>
2021-08-23 11:17:41 +0900

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+import { ApolloServer, gql } from 'apollo-server'
+
+
+const typeDefs = gql`
+  type User {
+    email: String!
+    avatar: String
+    friends: [User]!
+  }
+  type Query {
+    me: User!
+  }
+`
+
+const resolvers = {
+  Query: {
+    me() {
+      return {
+        email: "yoda@masters.com",
+        avatar: "http://yoda.ong",
+        friends: []
+      }
+    }
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+server.listen(4000).then(() => console.log("on port 4000"))
