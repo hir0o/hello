d5902b09f474aac26ac29b4d61e0e2d49f040080
hir0o <mk.9.q.kyu@gmail.com>
2021-08-23 16:25:27 +0900

    update

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import { ApolloServer, gql } from 'apollo-server'
-
+import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
   type User {
@@ -7,26 +6,72 @@ const typeDefs = gql`
     avatar: String
     friends: [User]!
   }
-  type Query {
-    me: User!
+
+  type Shue {
+    brand: String!
+    size: Int!
   }
-`
+
+  input ShueInput {
+    brand: String
+    size: Int
+  }
+
+  type Query {
+    user: User!
+    shoes(input: ShueInput): Shue!
+  }
+
+  input NesShueInput {
+    brand: String!
+    size: Int!
+  }
+
+  type Mutation {
+    newShue(input: ShueInput): Shue!
+  }
+`;
+
+const shues = [
+  {
+    brand: "nike",
+    size: 13,
+  },
+  {
+    brand: "puma",
+    size: 14,
+  },
+  {
+    brand: "adidas",
+    size: 12,
+  },
+];
 
 const resolvers = {
   Query: {
-    me() {
+    shoes(_, { input }) {
+      const shue = shues.find((sh) => sh.brand === input.brand);
+      console.log(shue);
+      return shue;
+    },
+    user() {
       return {
         email: "yoda@masters.com",
         avatar: "http://yoda.ong",
-        friends: []
-      }
-    }
-  }
-}
+        friends: [],
+      };
+    },
+  },
+  Mutation: {
+    newShue(_, { input }) {
+      return input;
+    },
+  },
+};
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
-})
+  resolvers,
+});
 
-server.listen(4000).then(() => console.log("on port 4000"))
+server.listen(4000).then(() => console.log("on port 4000"));
